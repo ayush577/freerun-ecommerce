9d153ce7f3a0964081f7532e68d926e065db47ab
ayush577 <agandhi62@gmail.com>
2024-07-24 15:50:06 +0530

    changes

--- components/atom/ReactQueryProvider/ReactQueryProvider.tsx
+++ components/atom/ReactQueryProvider/ReactQueryProvider.tsx
@@ -8,7 +8,19 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import { useState } from 'react'
 
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+)
+
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+
+  const [showDevtools, setShowDevTools] = useState(false)
+
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -26,9 +38,19 @@ const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
       }),
   )
 
+  React.useEffect(() => {
+    // @ts-expect-error
+    window.toggleDevtools = () => setShowDevTools((prev) => !prev)
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
         {children}
     </QueryClientProvider>
   )
